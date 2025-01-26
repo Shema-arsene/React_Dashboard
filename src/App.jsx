a065bc24d2fcf93d25f197d98a90b67c8b72b1b2
Shema-arsene <aniyonkurushema@gmail.com>
2025-01-26 04:25:16 +0200

    Implemented Card components and AreaChart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Sidebar from "./Components/Sidebar"
 import Navbar from "./Components/Navbar"
+import LeftColumn from "./Components/LeftColumn"
+import RightColumn from "./Components/RightColumn"
 
 const App = () => {
   return (
@@ -8,8 +10,15 @@ const App = () => {
       <Sidebar />
       <div className="flex flex-col flex-1 relative">
         <Navbar />
+        <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+          <div className="col-span-2">
+            <LeftColumn />
+          </div>
+          <div className="w-full">
+            <RightColumn />
+          </div>
+        </div>
       </div>
-      <div className=""></div>
     </main>
   )
 }
